dc183ff9b6976e76af6be962f80f730bb7dca44d
nhishon <nhishon@luc.edu>
2019-03-23 17:10:48 -0500

    authentication

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+//dev
+module.exports = {
+    googleClientID: "250946725945-68cj3d3npu6h4dp676rqj28rub393bvl.apps.googleusercontent.com",
+    googleClientSecret: "uXlf_rHDiBw8o4uBUAYEN1Pf",
+    mongoURI: 'mongodb+srv://nhishon:vmeZMDJIlRyMRQVD@tamagotchi-kaktl.mongodb.net/test?retryWrites=true',
+    cookieKey: 'asdhfajsdfahjdsfnakjsdfabnkpjd'
+};
+

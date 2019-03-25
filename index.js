46e44d752d077a0b446415b4eec4479644b0d5cd
nhishon <nhishon@luc.edu>
2019-03-24 20:46:36 -0500

    wrong directory push

--- index.js
+++ index.js
@@ -20,6 +20,14 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    const path = require ('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 mongoose.connect(keys.mongoURI);
 

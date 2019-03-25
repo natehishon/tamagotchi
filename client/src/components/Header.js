b9e477c4f495c965119ac2789c79fce11360273c
nhishon <nhishon@luc.edu>
2019-03-24 20:35:04 -0500

    login routing

---
+++ client/src/components/Header.js
@@ -0,0 +1,44 @@
+
+import { Navbar } from 'react-bulma-components';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+
+
+class Header extends Component {
+
+    renderContent(){
+        console.log(this.props.auth);
+        switch (this.props.auth) {
+            case null:
+                return ;
+
+            case false:
+                return <a href="auth/google" className="navbar-item">Login with Google</a>;
+
+            default:
+                return <a className="navbar-item" href="/api/logout">Logout</a>;
+        }
+    }
+    
+    render() {
+        return (
+            <nav className="navbar">
+                <div id="" className="navbar-menu">
+                    <div className="navbar-start">
+                        <Link to={this.props.auth ? '/tamagotchi' : '/'} className="navbar-item">Tamagotchi</Link>
+
+                        {this.renderContent()}
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
+
+function mapStateToProps({ auth }) {
+    return { auth: auth };
+}
+
+export default connect(mapStateToProps )(Header);
---
+++ client/src/components/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Menu = () => {
+
+    return (
+        <div>
+            Landing, logged out?
+        </div>
+    )
+
+};
+
+export default Menu;

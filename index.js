2746b7525dc09f41fbaa9aece36f7c1e55e32544
nhishon <nhishon@luc.edu>
2019-03-24 22:08:49 -0500

    index fix

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
+// mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
@@ -19,20 +21,26 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+require('./routes/authRoutes')(app);
 
-if(process.env.NODE_ENV === 'production'){
+
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like our main.js file, or main.css file!
     app.use(express.static('client/build'));
 
-    const path = require ('path');
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require('path');
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
-mongoose.connect(keys.mongoURI);
 
 
-require('./routes/authRoutes')(app);
+
+
 
 const PORT = process.env.PORT  || 5000;
 app.listen(PORT);
